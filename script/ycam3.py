4df9ff6b05993737a352914ae500f0e695161503
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2025-05-21 11:31:23 +0900

    Added 3D mesh

--- script/ycam3.py
+++ script/ycam3.py
@@ -11,8 +11,17 @@ from tf import transformations
 from smabo import tflib
 from rviz_tools_py import rviz_tools
 
+Config={
+  "mesh": ['smabo','mesh/Sx.stl',1000]
+}
+
+
 # Initialize the ROS Node
 rospy.init_node('marker_ycam', anonymous=False, log_level=rospy.INFO, disable_signals=False)
+try:
+  Config.update(rospy.get_param("/config/sensors"))
+except Exception as e:
+  print("get_param exception:",e.args)
 
 # Define exit handler
 def cleanup_node():
@@ -23,7 +32,11 @@ rospy.on_shutdown(cleanup_node)
 
 markers = rviz_tools.RvizMarkers('camera', 'ycam_marker')
 T1 = transformations.translation_matrix((0,0,0))
-sc1 = Vector3(1,1,1)
+try:
+  skl=float(Config["mesh"][2])
+except Exception:
+  skl=1
+sc1 = Vector3(skl,skl,skl)
 tr2=Transform()
 tr2.translation.z=530
 tr2.rotation.w=1.
@@ -31,7 +44,7 @@ T2=tflib.toRT(tr2)
 sc2 = Vector3(400,300,300)
 
 while not rospy.is_shutdown():
-  mesh_file1 = "package://smabo/mesh/YCAM3.stl"
+  mesh_file1 = "package://"+Config["mesh"][0]+"/"+Config["mesh"][1]
   markers.publishMesh(T1,mesh_file1,'white', sc1, 0.5)
   markers.publishCube(T2,(0.1,0.1,0.1,0.05), sc2, 0.5)
   rospy.Rate(5).sleep() #5Hz
